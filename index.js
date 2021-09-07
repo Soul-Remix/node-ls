155b7c41ab4ae9bcffb7ce0f73499d28c1feeab2
Soul-Remix <soulremix-webdev@pm.me>
2021-09-07 23:06:06 +0300

    Finished project

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
+const path = require('path');
 const chalk = require('chalk');
 
-fs.readdir(process.cwd(), async (err, files) => {
+const target = process.argv[2] || process.cwd();
+
+fs.readdir(target, async (err, files) => {
   if (err) {
     console.log(err);
     throw new Error(err);
   }
   const statPromises = files.map((file) => {
-    return fs.promises.lstat(file);
+    return fs.promises.lstat(path.join(target, file));
   });
   const arr = await Promise.all(statPromises);
   for (let stat of arr) {
